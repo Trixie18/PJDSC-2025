5c73da487f6ad7628390ae19a9ede3ef25757fe2
John Trixie Ocampo <jtocampo0118@gmail.com>
2025-10-21 15:30:21 +0800

    poslihed date picker to block impossible values

--- main.py
+++ main.py
@@ -1003,10 +1003,34 @@ def page_historical_analytics():
     with st.sidebar:
         st.markdown("### Selection Settings")
         col1, col2 = st.columns(2)
+        
+        # Initialize session state for dates if not exists
+        if 'hist_start_date' not in st.session_state:
+            st.session_state.hist_start_date = datetime(2025, 7, 20).date()
+        if 'hist_end_date' not in st.session_state:
+            st.session_state.hist_end_date = datetime(2025, 9, 12).date()
+        
         with col1:
-            start_date = st.date_input("Start Date", datetime(2025, 7, 20).date(), min_value=min_date, max_value=max_date, key='hist_start')
+            # Start date picker: end date and beyond are disabled
+            start_date = st.date_input(
+                "Start Date", 
+                value=st.session_state.hist_start_date,
+                min_value=min_date, 
+                max_value=st.session_state.hist_end_date,  # Can't go past end date
+                key='hist_start'
+            )
+            st.session_state.hist_start_date = start_date
+        
         with col2:
-            end_date = st.date_input("End Date", datetime(2025, 9, 12).date(), min_value=min_date, max_value=max_date, key='hist_end')
+            # End date picker: start date and before are disabled
+            end_date = st.date_input(
+                "End Date", 
+                value=st.session_state.hist_end_date,
+                min_value=st.session_state.hist_start_date,  # Can't go before start date
+                max_value=max_date,
+                key='hist_end'
+            )
+            st.session_state.hist_end_date = end_date
         
         selected_cities = st.multiselect("Select Cities", sorted(CITY_COORDINATES.keys()), default=['Manila', 'Quezon City'], key='hist_cities')
     
